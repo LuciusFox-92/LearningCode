ce9ae0c1ec4371475ad2e8ca3319e0bf155ac346
Uwais Patel <uwaispatel979@gmail.com>
2022-04-09 00:57:36 +0200

    👌 IMPROVE: Practiced JS Promise.all()

--- Javascript/Async JS/Promises/ConcurrentPromises.js
+++ Javascript/Async JS/Promises/ConcurrentPromises.js
@@ -4,19 +4,21 @@
 let promise1 = new Promise(function(resolve, reject) {
     setTimeout(function() {
         resolve("Promise 1");
+        console.log("Just resolved promise 1");
     }, 1000);
 });
 
 let promise2 = new Promise(function (resolve, reject) {
 	setTimeout(function () {
 		resolve("Promise 2");
+        console.log("Just resolved promise 2");
 	}, 1000);
 });
 
 let promise3 = new Promise(function (resolve, reject) {
 	setTimeout(function () {
     if(!true){
-        resolve("Promise 3");
+        resolve("Promise 3 fulfilled");
     }
     else {
         reject("Promise 3 error");
@@ -26,9 +28,16 @@ let promise3 = new Promise(function (resolve, reject) {
 
 
 //Use Promise.all to wait for all of them to complete.
+
+//Promise.all will return 1 of 2 things:
+//1. An array of all the resolved values 
+//2. A a single rejected promise
+
 Promise.all([promise1, promise2, promise3])
   .then(function (values) {
-  console.log(values);
+  values.forEach((value) => {
+      console.log(value);
+    });
   })
   .catch(function (error) {
   console.log(error);
